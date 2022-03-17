f5678ea2d5a845f25bbb0c465f9d36209880bb32
networkaj12 <networkaj12@gmail.com>
2022-03-17 16:30:58 +0530

    testing library

---
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, 64network and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('library_member', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ library_management/library_management/doctype/library_member/test_library_member.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, 64network and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class Testlibrary_member(unittest.TestCase):
+	pass
